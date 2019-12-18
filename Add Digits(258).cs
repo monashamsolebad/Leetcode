2de0b538538d768f18994964951ef0a2c57e95f0
monashamsolebad <47610427+monashamsolebad@users.noreply.github.com>
2019-12-18 09:56:45 -0800

    Create Add Digits(258).cs

---
+++ Add Digits(258).cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int AddDigits(int n) {
+        int sum = 0; 
+  
+        while (n > 0 || sum > 9)  
+        { 
+            if (n == 0) 
+            { 
+                n = sum; 
+                sum = 0; 
+            } 
+            sum += n % 10; 
+            n /= 10; 
+        } 
+        return sum; 
+    }
+}
