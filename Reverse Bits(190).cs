e55ff76d0c0ce47e38f8d76b2fd4bdf2514bf734
monashamsolebad <47610427+monashamsolebad@users.noreply.github.com>
2019-12-18 09:43:01 -0800

    Create Reverse Bits(190).cs

---
+++ Reverse Bits(190).cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public uint reverseBits(uint n) {
+        
+         uint res = 0;
+
+            for (int i = 0; i < 32; i++)             
+            {                 
+                var ibit = (n >> i) & 1;
+                res = (res << 1) + ibit;
+            }
+
+            return res;
+    }
+}
